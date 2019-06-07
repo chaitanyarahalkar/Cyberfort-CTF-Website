8a531978f2a289b4d764a6187cd3ad7422607870
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-06-07 18:58:07 +0530

    New files

---
+++ staticfiles/app/js/inspect.js
@@ -0,0 +1,12 @@
+
+function f1(){
+	var username =document.getElementById('username').value;
+    var password =document.getElementById('password').value;
+    if(username=="admin" && password=="admin@9999")
+     {
+     	alert("pict_CTF{j5_cl13n7_51d3}");
+     }
+     else{
+     	alert("Guessing won't work Trust me");
+     }
+}
