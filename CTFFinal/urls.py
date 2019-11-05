9ede3ce5ea46f5ac9a736ed2faf9225b1ddd063e
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-11-05 13:55:55 +0530

    Add sitemap

--- CTFFinal/urls.py
+++ CTFFinal/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     url(r'^challenge-login/',views.cookielogin),
     url(r'^challenge-login-1/',views.hiddenfield),
     url(r'^infinite/',TemplateView.as_view(template_name='app/infinite.html')),
+    url(r'^sitemap.xml',TemplateView.as_view(template_name='app/sitemap.xml'))
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
